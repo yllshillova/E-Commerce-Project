ffe93aebff20b9b1d58b3c14603ede5847f4d32d
yll <yllshillova@gmail.com>
2023-06-07 17:17:05 +0200

    Added a webhook, increased security of payments by enabling secret storage

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAgregate;
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace API.Controllers
 {
@@ -16,8 +18,10 @@ namespace API.Controllers
     {
         private readonly StoreContext _context;
         private readonly PaymentService _paymentService;
-        public PaymentsController(PaymentService paymentService, StoreContext context)
+        private readonly IConfiguration _config;
+        public PaymentsController(PaymentService paymentService, StoreContext context, IConfiguration config)
         {
+            _config = config;
             _paymentService = paymentService;
             _context = context;
             
@@ -47,5 +51,30 @@ namespace API.Controllers
             return basket.MapBasketToDto();
 
         }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json,Request.Headers["Stripe-Signature"], _config["StripeSettings:WhSecret"]);
+
+            var charge = (Charge)stripeEvent.Data.Object;
+
+            var order = await _context.Orders.FirstOrDefaultAsync(x => x.PaymentIntentId == charge.PaymentIntentId);
+
+            if(charge.Status == "succeeded"){
+                 order.OrderStatus = OrderStatus.PaymentReceived;
+            }
+
+            await _context.SaveChangesAsync();
+            // kta ebojm return qe me informu stripe qe e kem pranu kit request perndryshe ky vazhdon disa dit tu dergu 
+            return new EmptyResult();
+
+
+        }
+
+
+
     }
 }
