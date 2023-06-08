89b721aeaadc4dc847cc287c7cf50681b84e136c
yll <yllshillova@gmail.com>
2023-06-08 15:14:54 +0200

    Product CRUD implemented, added automapper and image service

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,12 @@
 using System.Text.Json;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.RequesHelpers;
+using API.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,8 +15,12 @@ namespace API.Controllers
     public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IMapper _mapper;
+        private readonly ImageService _imageService;
+        public ProductsController(StoreContext context, IMapper mapper, ImageService imageService)
         {
+            _imageService = imageService;
+            _mapper = mapper;
             _context = context;
         }
 
@@ -29,7 +37,7 @@ namespace API.Controllers
             Response.AddPaginationHeader(products.MetaData);
             return products;
         }
-        [HttpGet("{id}")] // ky eshte kerkesa per me marr vetem nje produkt te caktuar {id} eshte id-ja e produktit qe do t jepet ex api/products/3
+        [HttpGet("{id}", Name = "GetProduct")] // ky eshte kerkesa per me marr vetem nje produkt te caktuar {id} eshte id-ja e produktit qe do t jepet ex api/products/3
 
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
@@ -44,10 +52,93 @@ namespace API.Controllers
             var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
             var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
 
-            return Ok(new {brands,types});
+            return Ok(new { brands, types });
 
         }
 
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromForm] CreateProductDto productDto)
+        {
+            // te map nkto <> deri ku po shkojm edhe nkllapa tani prej ku po dojm me map
+            var product = _mapper.Map<Product>(productDto);
+
+            if (productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if (imageResult.Error != null) return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+            }
+
+            _context.Products.Add(product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
+            return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
+        {
+            var product = await _context.Products.FindAsync(productDto.Id);
+            if (product == null) return NotFound();
+
+            _mapper.Map(productDto, product);
+            if (productDto.File != null)
+            {
+                // attempt to upload the image
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+                // nese fail me i shfaq bad request
+                if (imageResult.Error != null)
+                {
+                    return BadRequest(new ProblemDetails { Title = imageResult.Error.Message });
+                }
+                // kqyr a ka pas naj figur ma heret qe ka pas e fshin ata per me mujt me vendos t rejen 
+                if (!string.IsNullOrEmpty(product.PublicId))
+                {
+                    await _imageService.DeleteImageAsync(product.PublicId);
+                }
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+            }
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok(product);
+
+            return BadRequest(new ProblemDetails { Title = "Problem updating the product" });
+
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            if (!string.IsNullOrEmpty(product.PublicId))
+            {
+                await _imageService.DeleteImageAsync(product.PublicId);
+            }
+
+
+            _context.Products.Remove(product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest(new ProblemDetails { Title = "Problem deleting the product" });
+
+        }
 
     }
 }
